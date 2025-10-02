48c6941fc043ff971e86718b8a01c45df0a88133
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2025-10-02 20:17:03 -0300

    adicaoClassesFurnas_02-10_20:15

--- classes/classesFurnas.ts
+++ classes/classesFurnas.ts
@@ -17,3 +17,19 @@ class ConcetracaoGasSedimento extends Entidade {
     return this.idconcentracaogassed;
   }
 }
+
+class CamaraSolo extends Entidade {
+  idcamarasolo!: number;
+
+  getId(): number {
+    return this.idcamarasolo;
+  }
+}
+
+class Carbono extends Entidade {
+  idcarbono!: number;
+
+  getId(): number {
+    return this.idcarbono;
+  }
+}
