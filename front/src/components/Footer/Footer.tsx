7c8f3452f55915c33cafc2f5e3e17102c00b37c7
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-10-04 23:48:46 -0300

    ajustesSprint1_04-10_23:48

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -4,9 +4,16 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footerContainer}>
-            <p>
-                © 2025 LimnoVis. Desenvolvido por <a href="https://github.com" target="_blank" rel="noopener noreferrer">CrimsonCode</a>.
-            </p>
+
+            <div className='footer-container'>
+                <div className='footer-crimsoncode'>
+                    <h3>CRIMSONCODE</h3>
+                    <p>© 2025 LimnoVis. Desenvolvido por <a href="https://github.com" target="_blank" rel="noopener noreferrer">CrimsonCode</a></p>
+                </div>
+                <div className='footer-contatos'>
+                    <h3>Contatos</h3>
+                </div>
+            </div>
         </footer>
     );
 }
