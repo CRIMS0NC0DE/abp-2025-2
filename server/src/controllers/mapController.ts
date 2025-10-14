9be87458dc390309ae5d919cde65e90ea6306f10
Davi Snaider <davisnaider06@gmail.com>
2025-10-13 23:19:40 -0300

    feat: add integracao com mapa interativo

---
+++ server/src/controllers/mapController.ts
@@ -0,0 +1,65 @@
+
+
+import { Request, Response, NextFunction } from 'express';
+import { furnasPool, simaPool, balcarPool } from '../configs/db';
+
+
+interface PointProperties {
+  id: number | string;
+  nome: string;
+  tipo: string;
+}
+
+interface GeoJSONFeature {
+  type: 'Feature';
+  geometry: {
+    type: 'Point';
+    coordinates: [number, number];
+  };
+  properties: PointProperties;
+}
+
+//GeoJSON
+function formatQueryToGeoJSON(rows: any[], tipo: string): GeoJSONFeature[] {
+  return rows
+    .filter(row => row.lat != null && row.lng != null) // Garante que temos coordenadas
+    .map(row => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [parseFloat(row.lng), parseFloat(row.lat)], // [longitude, latitude]
+      },
+      properties: {
+        id: row.id,     // A query deve retornar uma coluna 'id'
+        nome: row.nome, // A query deve retornar uma coluna 'nome'
+        tipo: tipo,
+      },
+    }));
+}
+
+
+export const getGeographicPoints = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let allFeatures: GeoJSONFeature[] = [];
+
+   const [resFurnas, resSima, resBalcar] = await Promise.all([
+      furnasPool.query('SELECT idsitio as id, nome, lat, lng FROM TBSITIO'),
+      simaPool.query('SELECT idestacao as id, rotulo as nome, lat, lng FROM TBESTACAO'),
+      balcarPool.query('SELECT idsitio as id, nome, lat, lng FROM TBSITIO')
+    ]);
+
+    allFeatures.push(...formatQueryToGeoJSON(resBalcar.rows, 'sitio_balcar'));
+    allFeatures.push(...formatQueryToGeoJSON(resFurnas.rows, 'sitio_furnas'));
+    allFeatures.push(...formatQueryToGeoJSON(resSima.rows, 'estacao_sima'));
+
+    const geoJSONResponse = {
+      type: 'FeatureCollection',
+      features: allFeatures,
+    };
+
+    return res.status(200).json(geoJSONResponse);
+  } catch (error) {
+    
+    next(error);
+  }
+};
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,21 @@ import express from "express";
 import balcar from "./balcar";
 import furnas from "./furnas";
 import sima from "./sima";
+import { getGeographicPoints } from '../controllers/mapController';
 
 const router = express.Router();
 
+router.get("/teste", (req, res) => {
+  console.log("ROTA DE TESTE ACESSADA COM SUCESSO!"); // Mensagem para o terminal
+  res.status(200).json({ message: "A rota de teste funcionou!" });
+});
+
+
+
+router.get('/api/pontos-geograficos', getGeographicPoints);
 router.use("/balcar", balcar);
 router.use("/furnas", furnas);
 router.use("/sima", sima);
+
 
 export default router;
