7683bd0940441a0cd10569e119f72e21f1c377e2
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-10-14 21:29:06 -0300

    revisãoFooter_14-10_21:27

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -3,35 +3,20 @@ import styles from './Footer.module.css';
 // Footer simples com informações de direitos autorais.
 export default function Footer() {
     return (
-        <footer className={styles.footerContainer}>
-            <div className='inpe__container'>
+        <footer>
+            <div className={styles.inpe__container}>
                 <h3>INPE</h3>
-                <ul>
-                    <li>
-                        <a href="">Site</a>
-                    </li>
-                    <li>
-                        <a href="">Instagram</a>
-                    </li>
-                </ul>
+                <a href="">Instagram</a>
+                <a href="">Site</a>
             </div>
-            <div className='furnas__container'>
-                <h3>Furnas</h3>
-                <ul>
-                    <li>
-                        <a href="" className='furnas__container__links'>Site</a>
-                    </li>
-                </ul>
+            <div className={styles.furnas__container}>
+                <h3>FURNAS</h3>
+                <a href="">Site</a>
             </div>
-            <div className='crimsoncode__container'>
-                <h3>Crimson Code</h3>
-                <ul>
-                    <li>
-                        <a href="">GitHub</a>
-                        <a href="">Fale Conosco</a>
-                        <a href="">Instagram</a>
-                    </li>
-                </ul>
+            <div className={styles.crimson__container}>
+                <h3>CRIMS0NC0DE</h3>
+                <a href="">GitHub</a>
+                <a href="">Fale Conosco</a>
             </div>
         </footer>
     );
