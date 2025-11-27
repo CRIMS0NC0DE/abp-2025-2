bf0fd732f0c8ecd4a5420c637b7cfc840957893d
Bruno Mark <115597145+bruno-mark@users.noreply.github.com>
2025-11-27 13:01:39 -0300

    Fix import path for campoPorTabela.controller

--- server/src/routes/furnas/campoPorTabela.routes.ts
+++ server/src/routes/furnas/campoPorTabela.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getAll, getById, exportData} from "../../controllers/furnas/campoPorTabela.controller"
+import {getAll, getById, exportData} from "../../controllers/furnas/campoportabela.controller"
 
 const router = Router();
 
