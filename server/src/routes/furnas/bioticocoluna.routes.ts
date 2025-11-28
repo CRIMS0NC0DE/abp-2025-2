8b0f777aa6b40c4d1778149a9b74939a6cee05f7
Bruno Mark <115597145+bruno-mark@users.noreply.github.com>
2025-11-28 08:19:46 -0300

    Fix import path for bioticoColuna controller

--- server/src/routes/furnas/bioticocoluna.routes.ts
+++ server/src/routes/furnas/bioticocoluna.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAll, getById, exportData } from "../../controllers/furnas/bioticoColuna.controller";
+import { getAll, getById, exportData } from "../../controllers/furnas/bioticocoluna.controller";
 
 const router = Router();
 
